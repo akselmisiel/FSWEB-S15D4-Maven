4b370f08b4c7e82c341df1f3237b152dcef4b02d
Aksel <akselmisiel@gmail.com>
2024-06-26 19:05:28 +0300

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,26 @@
 package org.example;
 
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
+    public static boolean checkForPalindrome(String str) {
+        String cleanedStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversedStr = new StringBuilder(cleanedStr).reverse().toString();
+
+        return cleanedStr.equals(reversedStr);
+    }
+
+    public static String convertDecimalToBinary(int decimal) {
+        LinkedList<Integer> binary = new LinkedList<>();
+        while (decimal > 0) {
+            binary.addFirst(decimal % 2);
+            decimal = decimal / 2;
+        }
+        return binary.toString().replaceAll("[^0-1]", "");
+    }
 }
